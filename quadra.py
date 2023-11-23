5e6896db9d141ee748221d163cc2dd47d2e569c4
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2023-11-23 22:25:52 +0100

    Fonctional quadra parser/renderer

---
+++ quadra.py
@@ -0,0 +1,88 @@
+import sys, os
+from collections import OrderedDict
+
+from quadra_defs import entry_fields
+
+class QuadraFile:
+    def __init__(self, filename):
+        self.filename = filename
+        self.lines = []
+
+    @classmethod
+    def parse(cls, filename):
+        ret = cls(filename)
+        with open(filename, 'r') as fin:
+            for line in fin:
+                ret.lines.append(QuadraLine.parse(line))
+        return ret
+
+    def append(self, line):
+        self.lines.append(line)
+
+    def save(self):
+        with open(self.filename, 'w') as fout:
+            fout.write('\n'.join(map(QuadraLine.encode, self.lines)))
+
+class QuadraLine:
+    def __init__(self):
+        self.fields = OrderedDict()
+        self.mandatory = {}
+
+        for _key, _name, _start, _len, _mandatory in entry_fields:
+            self.fields[_key] = ''
+            self.mandatory[_key] = _mandatory
+
+    @classmethod
+    def parse(cls, line):
+        ret = cls()
+        ret.decode(line)
+        return ret
+
+    def decode(self, line):
+        for _key, _name, _start, _len, _mandatory in entry_fields:
+            _value = line[_start - 1 : _start - 1 +_len]
+
+            self.fields[_key] = _value
+
+    def encode(self):
+        # Create empty string with max field size
+        last_start, last_len = entry_fields[-1][2:4]
+        ret = [' '] * (last_start + last_len - 1)
+        for _key, _name, _start, _len, _mandatory in entry_fields:
+            # Apply the maximum pading, then truncate the result to fit in all cases
+            t_field = (self.fields[_key] + ' '*_len)[:_len]
+            ret[_start - 1: _start + _len - 1] = t_field
+
+        return ''.join(ret)
+
+    def __str__(self):
+        ret = []
+        for k,v in self.fields.items():
+            ret.append('{}{} : {}'.format('*' if self.mandatory[k] else '' ,k, v))
+
+        return '\n'.join(ret)
+
+def run():
+    quadra_file = QuadraFile.parse('Fichier qui fonctionnait avant.TXT')
+    print(quadra_file.lines[0])
+
+    line = QuadraLine()
+    line.fields['date_ecriture'] = '150684'
+    line.fields['libelle_30'] = 'There is a good side to every situation'
+    print(line.encode())
+    
+    quadra_file = QuadraFile('new_quadra_file.txt')
+    line = QuadraLine()
+    line.fields['date_ecriture'] = '150684'
+    line.fields['libelle_30'] = 'There is a good side to every situation'
+    quadra_file.append(line)
+    line = QuadraLine()
+    line.fields['date_ecriture'] = '051015'
+    line.fields['libelle_30'] = 'Hello world'
+    quadra_file.append(line)
+
+    quadra_file.save()
+
+if __name__ == '__main__':
+    run()
+
